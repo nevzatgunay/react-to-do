f31c0b23889c326ba19a872e2cfc272e05097623
nevzatgunay <gunaynevzat@yandex.com>
2018-11-14 14:31:07 +0300

    added: new list and adding to the list

--- src/App.js
+++ src/App.js
@@ -8,19 +8,50 @@ class App extends Component {
     super();
 
     this.state = {
+
+      newTodo: '',
+
       todos : [{
-        id: 1, name:'Wordpress Developer'
+        id: 1, name:'Wash the dishes'
       },
       {
-        id: 2, name:'React Developer' 
+        id: 2, name:'go to the gym' 
       },
       {
-        id: 3, name:'NodeJS Developer'
+        id: 3, name:'read your book'
       }]
     };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+
+    this.setState({
+      newTodo: event.target.value
+    });
+  }
+
+  addTodo(){
+    const newTodo = {
+      name: this.state.newTodo,
+      id: this.state.todos[this.state.todos.length - 1].id + 1
+    };
+
+    const todos = this.state.todos;
+    todos.push(newTodo);
+
+    this.setState({
+      todos: todos,
+      newTodo: ''
+    });
   }
 
   render() {
+
+    console.log(this.state.newTodo);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -30,6 +61,21 @@ class App extends Component {
           </p>
         </header>
         <div className="container">
+          <input 
+            type="text" 
+            name="todo"
+            className="m-4 form-control" 
+            placeholder="Add a new to-do"
+            value={ this.state.newTodo }
+            onChange={ this.handleChange }/>
+
+          <button 
+            onClick={ this.addTodo }
+            className="btn-info mb-3 form-control">
+            Add a to-do
+          </button>
+          
+
           <ul className="list-group">
             { 
               this.state.todos.map((item) => {
